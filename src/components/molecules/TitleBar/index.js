534d224550270832b486bb6a7da68f2fc252252d
ryanking13 <def6488@gmail.com>
2018-05-13 02:18:16 +0900

    make titlebar load userinfo on each refesh

--- src/components/molecules/TitleBar/index.js
+++ src/components/molecules/TitleBar/index.js
@@ -3,42 +3,95 @@ import * as colors from 'material-ui/styles/colors'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 import { connect } from 'react-redux'
-import { changeRoute, signOut } from '../../../store/user/actions'
+import { changeRoute, signOut, getUserProfile } from '../../../store/user/actions'
 
-const TitleBar = ({ logged, onClickSignIn, onClickSignOut }) => {
-  const onClickRightIconButton = () => {
-    if (logged === true) onClickSignOut()
-    else onClickSignIn()
+class TitleBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onClickRightIconButton = this.onClickRightIconButton.bind(this)
   }
 
-  return (
-    <AppBar
-      title={'SNUwagon'}
-      showMenuIconButton={false}
-      iconElementRight={logged === true ? <FlatButton>Sign out</FlatButton> : <FlatButton>Sign in</FlatButton>}
-      onRightIconButtonClick={onClickRightIconButton}
-      titleStyle={{
-        textAlign: 'center',
-        fontSize: '40',
-      }}
-      style={{
-        backgroundColor: colors.indigo500,
-        height: '250px',
-      }}
-    />
-  )
+  componentDidMount() {
+    this.props.loadProfile()
+  }
+
+  onClickRightIconButton() {
+    if (this.props.logged === true) this.props.onClickSignOut()
+    else this.props.onClickSignIn()
+  }
+
+  render() {
+    const profiles = (
+      <div>
+        <FlatButton style={{ color: 'white' }}>{ this.props.profile.username }</FlatButton>
+        <FlatButton style={{ color: 'white' }}>{ this.props.profile.credit }</FlatButton>
+        <FlatButton style={{ color: 'white' }}>Sign out</FlatButton>
+      </div>
+    )
+
+    return (
+      <AppBar
+        title={'SNUwagon'}
+        showMenuIconButton={false}
+        iconElementRight={this.props.logged === true ? profiles : <FlatButton>Sign in</FlatButton>}
+        onRightIconButtonClick={this.onClickRightIconButton}
+        titleStyle={{
+          textAlign: 'center',
+          fontSize: '40',
+        }}
+        style={{
+          backgroundColor: colors.indigo500,
+        }}
+      />
+    )
+  }
 }
+
+// const TitleBar = ({ logged, profile, onClickSignIn, onClickSignOut }) => {
+//   const onClickRightIconButton = () => {
+//     if (logged === true) onClickSignOut()
+//     else onClickSignIn()
+//   }
+//
+//   const profiles = (
+//     <div>
+//       <FlatButton style={{ color: 'white' }}>{ profile.username }</FlatButton>
+//       <FlatButton style={{ color: 'white' }}>{ profile.credit }</FlatButton>
+//       <FlatButton style={{ color: 'white' }}>Sign out</FlatButton>
+//     </div>
+//   )
+//
+//   return (
+//     <AppBar
+//       title={'SNUwagon'}
+//       showMenuIconButton={false}
+//       iconElementRight={logged === true ? profiles : <FlatButton>Sign in</FlatButton>}
+//       onRightIconButtonClick={onClickRightIconButton}
+//       titleStyle={{
+//         textAlign: 'center',
+//         fontSize: '40',
+//       }}
+//       style={{
+//         backgroundColor: colors.indigo500,
+//       }}
+//     />
+//   )
+// }
 
 TitleBar.propTypes = {
   logged: PropTypes.bool,
+  profile: PropTypes.object,
   onClickSignIn: PropTypes.func,
   onClickSignOut: PropTypes.func,
+  loadProfile: PropTypes.func,
   reverse: PropTypes.bool,
 }
 
 const mapStateToProps = (state) => {
   return {
     logged: state.user.login,
+    profile: state.user.profile,
   }
 }
 
@@ -50,6 +103,9 @@ const mapDispatchToProps = (dispatch) => {
     },
     onClickSignOut: () => {
       dispatch(signOut())
+    },
+    loadProfile: () => {
+      dispatch(getUserProfile())
     },
   }
 }
