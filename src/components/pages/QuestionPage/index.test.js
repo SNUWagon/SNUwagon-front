7d5cf8f88fad1669b76117183b4475e5ecc960cd
ddoyoon <ddoyoon.kim@gmail.com>
2018-05-14 01:34:25 +0900

    Add test for QuestionPage

--- src/components/pages/QuestionPage/index.test.js
+++ src/components/pages/QuestionPage/index.test.js
@@ -1,7 +1,32 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
 import QuestionPage from '.'
 
+const mockStore = configureStore([])
+
 it('renders', () => {
-  shallow(<QuestionPage />)
+  const initialState = {
+    user: {
+      login: false,
+      profile: {
+        userId: undefined,
+        username: undefined,
+        credit: undefined,
+      },
+    },
+    question: {
+      postId: undefined,
+      title: undefined,
+      content: undefined,
+      due: undefined,
+      bounty: undefined,
+      author: undefined,
+      resolved: undefined,
+      selected: undefined,
+      tags: undefined,
+    },
+  }
+  const store = mockStore(initialState)
+  shallow(<QuestionPage store={store} />)
 })
