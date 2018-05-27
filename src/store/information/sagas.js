e515b1aa410276b921f343afbd2867abe13668c1
ddoyoon <ddoyoon.kim@gmail.com>
2018-05-27 14:18:01 +0900

    Add information in store

---
+++ src/store/information/sagas.js
@@ -0,0 +1,82 @@
+import { take, call, fork, put } from 'redux-saga/effects'
+import { push } from 'react-router-redux'
+import api from 'services/api'
+import * as actions from './actions'
+
+const baseUrl = ''
+const informationUrl = `${baseUrl}/posts/information`
+
+export function* handleChangeRoute(newRoute) {
+  yield put(push(newRoute))
+}
+
+export function* handleWriteInformationPost(title, content, hiddenExist, hiddenContent, hiddenContentCost, hiddenBought, due, author, sponsorCredit, tags) {
+  const data = { title, content, username: author, tag: tags, hidden_content_cost: hiddenContentCost, hidden_exist: hiddenExist, hidden_content: hiddenContent, hidden_bought: hiddenBought, due, sponsor_credit: sponsorCredit }
+  const response = yield call(api.post, `${informationUrl}`, data)
+  if (response.success === true) {
+    yield put(actions.changeRoute(`/information/${response.data.id}`))
+  }
+}
+
+export function* handleGetInformationPost(postId) {
+  const response = yield call(api.get, `${informationUrl}/${postId}`)
+  if (response.success === true) {
+    yield put(actions.updateInformationPost(
+      response.data.id,
+      response.data.title,
+      response.data.content,
+      response.data.hidden_exist,
+      response.data.hidden_content,
+      response.data.hidden_content_cost,
+      response.data.hidden_bought,
+      response.data.due,
+      response.data.author,
+      response.data.tags,
+    ))
+  }
+}
+
+export function* handlePurchaseInformationPost(postId) {
+  const data = {}
+  const response = yield call(api.patch, `${informationUrl}/${postId}`, data)
+  console.log(response)
+  if (response.success === true) {
+    // console.log(response)
+  }
+}
+
+/* watcher functions */
+function* watchChangeRoute() {
+  while (true) {
+    const { route } = yield take(actions.CHANGE_ROUTE)
+    yield call(handleChangeRoute, route)
+  }
+}
+
+function* watchWriteInformationPost() {
+  while (true) {
+    const { title, content, hiddenExist, hiddenContent, hiddenContentCost, hiddenBought, due, author, sponsorCredit, tags } = yield take(actions.WRITE_INFORMATION_POST)
+    yield call(handleWriteInformationPost, title, content, hiddenExist, hiddenContent, hiddenContentCost, hiddenBought, due, author, sponsorCredit, tags)
+  }
+}
+
+function* watchGetInformationPost() {
+  while (true) {
+    const { postId } = yield take(actions.GET_INFORMATION_POST)
+    yield call(handleGetInformationPost, postId)
+  }
+}
+
+function* watchPurchaseInformationPost() {
+  while (true) {
+    const { postId } = yield take(actions.PURCHASE_INFORMATION_POST)
+    yield call(handlePurchaseInformationPost, postId)
+  }
+}
+
+export default function* () {
+  yield fork(watchChangeRoute)
+  yield fork(watchWriteInformationPost)
+  yield fork(watchGetInformationPost)
+  yield fork(watchPurchaseInformationPost)
+}
