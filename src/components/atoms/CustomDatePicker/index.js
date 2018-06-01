fe074d0e7afa6d98c146c45dfb1aee8d3362f967
ddoyoon <ddoyoon.kim@gmail.com>
2018-06-01 19:27:27 +0900

    Fix CustomDatePicker to include time component, modify test

--- src/components/atoms/CustomDatePicker/index.js
+++ src/components/atoms/CustomDatePicker/index.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import DatePicker from 'material-ui/DatePicker'
+import TimePicker from 'material-ui/TimePicker'
 
 const today = new Date()
 const maxDay = new Date()
@@ -9,35 +10,61 @@ export default class CustomDatePicker extends React.Component {
 
   constructor(props) {
     super(props)
-
+    this.newDate = new Date()
     this.state = {
-      due: null,
+      dueDate: null,
+      dueTime: null,
     }
+    this.handleDateChange = this.handleDateChange.bind(this)
+    this.handleTimeChange = this.handleTimeChange.bind(this)
   }
 
-  handleChange = (event, date) => {
+  handleDateChange = (event, date) => {
     this.setState({
-      due: date,
+      dueDate: date,
     })
-    this.props.onChange(date)
+    this.newDate.setFullYear(date.getFullYear())
+    this.newDate.setMonth(date.getMonth())
+    this.newDate.setDate(date.getDate())
+    if (this.state.dueTime !== null) this.props.onChange(this.newDate)
+  }
+
+  handleTimeChange = (event, time) => {
+    this.setState({
+      dueTime: time,
+    })
+    this.newDate.setHours(time.getHours())
+    this.newDate.setMinutes(time.getMinutes())
+    this.newDate.setSeconds(0)
+    if (this.state.dueDate !== null) this.props.onChange(this.newDate)
   }
 
   render() {
     return (
-      <DatePicker
-        className={'due-input'}
-        autoOk
-        hintText="Select Due Date"
-        value={this.state.due}
-        minDate={today}
-        maxDate={maxDay}
-        onChange={this.handleChange}
-      />
+      <div>
+        <DatePicker
+          className={'due-date-input'}
+          autoOk
+          hintText="Select Due Date"
+          value={this.state.dueDate}
+          minDate={today}
+          maxDate={maxDay}
+          onChange={this.handleDateChange}
+        />
+        <TimePicker
+          className={'due-time-input'}
+          defaultTime={today.hours}
+          hintText="Select Due Time"
+          minutesStep={10}
+          onChange={this.handleTimeChange}
+        />
+      </div>
     )
   }
 }
 
 CustomDatePicker.propTypes = {
-  handleChange: PropTypes.func,
+  handleDateChange: PropTypes.func,
+  handleTimeChange: PropTypes.func,
   onChange: PropTypes.func,
 }
--- src/components/atoms/CustomDatePicker/index.test.js
+++ src/components/atoms/CustomDatePicker/index.test.js
@@ -6,31 +6,53 @@ import CustomDatePicker from '.'
 const wrap = (props = {}) => shallow(<CustomDatePicker {...props} />)
 
 const mockStore = configureStore([])
-
+const initialState = {
+  dueDate: null,
+  dueTime: null,
+}
+const store = mockStore(initialState)
 
 it('renders', () => {
-  const initialState = {
-    due: null,
-  }
-  const store = mockStore(initialState)
   const props = {
     store,
-    due: '0001-01-01T09:29:00+08:28',
+    state: {
+      dueDate: '0001-01-01T09:29:00+08:28',
+      dueTime: '0001-01-01T09:29:00+08:28',
+    },
   }
   wrap(props)
 })
 
-it('calls handleChange on change', () => {
-  const initialState = {
-    due: null,
-  }
-  const store = mockStore(initialState)
+it('calls onChange, selecting date then time', () => {
   const props = {
     store,
-    due: '0001-01-01T09:29:00+08:28',
+    state: {
+      dueDate: '2018-01-02T09:29:00+08:28',
+      dueTime: '2018-01-02T09:29:00+08:28',
+    },
+    newDate: undefined,
     onChange: jest.fn(),
   }
   const wrapper = wrap(props)
-  wrapper.find('.due-input').simulate('change', ('event', { date: '0001-01-02T09:29:00+08:28' }))
+  wrapper.find('.due-date-input').simulate('change', 'event', new Date('2018-01-02T09:29:00+08:28'))
+  expect(props.onChange).not.toHaveBeenCalled()
+  wrapper.find('.due-time-input').simulate('change', 'event', new Date('0001-01-02T09:29:00+08:28'))
+  expect(props.onChange).toHaveBeenCalled()
+})
+
+it('calls onChange, changing time then date', () => {
+  const props = {
+    store,
+    state: {
+      dueDate: '2018-01-02T09:29:00+08:28',
+      dueTime: '2018-01-02T09:29:00+08:28',
+    },
+    newDate: undefined,
+    onChange: jest.fn(),
+  }
+  const wrapper = wrap(props)
+  wrapper.find('.due-time-input').simulate('change', 'event', new Date('2018-01-02T09:29:00+08:28'))
+  expect(props.onChange).not.toHaveBeenCalled()
+  wrapper.find('.due-date-input').simulate('change', 'event', new Date('0001-01-02T09:29:00+08:28'))
   expect(props.onChange).toHaveBeenCalled()
 })
