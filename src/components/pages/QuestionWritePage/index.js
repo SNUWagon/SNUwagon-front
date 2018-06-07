5538aa11d51821e0d4684b2db8b8b1dfa335ef15
ddoyoon <ddoyoon.kim@gmail.com>
2018-06-07 16:24:18 +0900

    Add fail modal to QuestionWrite

--- src/components/pages/QuestionWritePage/index.js
+++ src/components/pages/QuestionWritePage/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import QuestionWrite from '../../../containers/QuestionWrite'
 import TitleBar from '../../../components/molecules/TitleBar'
+import BaseModal from '../../../containers/modals/BaseModal'
 
 const QuestionWritePage = () => {
   return (
     <div>
       <TitleBar />
       <QuestionWrite />
+      <BaseModal />
     </div>
   )
 }
